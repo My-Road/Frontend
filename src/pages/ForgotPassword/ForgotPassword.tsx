29766786c95c2112d9a4741b6e1ff24e2425e6f4
BaselTurkman <Basel.Turkman123@gmail.com>
2025-03-10 00:11:49 +0200

    Update to use FormikProvider

--- src/pages/ForgotPassword/ForgotPassword.tsx
+++ src/pages/ForgotPassword/ForgotPassword.tsx
@@ -1,4 +1,4 @@
-import { Formik, Form } from "formik";
+import { Form, FormikProvider, useFormik } from "formik";
 import * as yup from "yup";
 import { Button, Paper, Typography, Box } from "@mui/material";
 import Grid from "@mui/material/Grid2";
@@ -18,57 +18,55 @@ const schema = yup.object({
 const ForgetPassword: React.FC = () => {
   const navigate = useNavigate();
 
-  const handleSubmit = (values: ResetPasswordPayLoad) => {
+  const onSubmit = (values: ResetPasswordPayLoad) => {
+    //for now 
     console.log("Form Data:", values);
     navigate(`/reset-password`);
-  };
+  }
 
-  const formikConfig = {
+  const formik = useFormik({
     initialValues: { email: "" },
     validationSchema: schema,
-    onSubmit: handleSubmit,
-  };
+    onSubmit
+  });
 
   return (
-    <Formik {...formikConfig}>
-      {({ handleSubmit }) => (
-        <Grid
-          container
-          justifyContent="center"
-          alignItems="center"
-          sx={{ minHeight: "100vh" }}
-        >
-          <Grid size={{ xs: 10, sm: 8, md: 4 }}>
-            <Paper elevation={3} sx={{ padding: 3 }}>
-              <Box
-                sx={{
-                  clipPath: "inset(10px 10px 10px 10px);",
-                  textAlign: "center",
-                }}
+    <FormikProvider value={formik}>
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        sx={{ minHeight: "100vh" }}
+      >
+        <Grid size={{ xs: 10, sm: 8, md: 4 }}>
+          <Paper elevation={3} sx={{ padding: 3 }}>
+            <Box
+              sx={{
+                clipPath: "inset(10px 10px 10px 10px);",
+                textAlign: "center",
+              }}
+            >
+              <img src={Logo} alt="" width="150px" height="150px" />
+            </Box>
+            <Typography variant="h5" gutterBottom align="center">
+              Reset Password
+            </Typography>
+            <Form onSubmit={formik.handleSubmit}>
+              <TextField name="email" aria-label="enter a valid email" />
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                fullWidth
+                sx={{ mt: 2 }}
               >
-                <img src={Logo} alt="" width="150px" height="150px" />
-              </Box>
-              <Typography variant="h5" gutterBottom align="center">
                 Reset Password
-              </Typography>
-              {/* the error msg is "TextfieldErrors.Email is required" don't know why :3 */}
-              <Form onSubmit={handleSubmit}>
-                <TextField name="email" aria-label="enter a valid email" />
-                <Button
-                  type="submit"
-                  variant="contained"
-                  color="primary"
-                  fullWidth
-                  sx={{ mt: 2 }}
-                >
-                  Reset Password
-                </Button>
-              </Form>
-            </Paper>
-          </Grid>
+              </Button>
+            </Form>
+          </Paper>
         </Grid>
-      )}
-    </Formik>
+      </Grid>
+    </FormikProvider>
   );
 };
 
