16feb449e2df6df102be418514df47c9bf7db625
BaselTurkman <Basel.Turkman123@gmail.com>
2025-03-19 01:11:28 +0200

    Update Email Error Messagess

--- src/routes/imports.tsx
+++ src/routes/imports.tsx
@@ -5,5 +5,5 @@ export const NotFound = lazy(() => import("@/pages/NotFound"));
 export const Unauthenticated = lazy(() => import("@/pages/Unauthenticated"));
 export const Login = lazy(() => import("@/pages/Login"));
 export const Home = lazy(() => import("@/pages/Home"));
-export const ResetPassword = lazy(() => import("@/pages/ResetPassword"))
-export const ForgotPassword = lazy(() => import("@/pages/ForgotPassword"))
+export const ResetPassword = lazy(() => import("@/pages/ResetPassword"));
+export const ForgotPassword = lazy(() => import("@/pages/ForgotPassword"));
