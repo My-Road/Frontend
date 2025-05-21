ca03b5b1fb33863005f3d10f5d5168641da2d60c
BaselTurkman <Basel.Turkman123@gmail.com>
2025-05-21 23:03:47 +0300

    DP-40 Handle phone number for customer details & supplier details

--- src/pages/CustomerDetails/components/PersonalCustomerInfo/formSchema.ts
+++ src/pages/CustomerDetails/components/PersonalCustomerInfo/formSchema.ts
@@ -6,7 +6,7 @@ export const validationSchema = yup.object({
     .required("Please enter your full name")
     .min(2, "Full name must be at least 2 characters"),
 
-    email: yup
+  email: yup
     .string()
     .email("Please enter a valid email")
     .required("Please enter your email")
@@ -15,7 +15,9 @@ export const validationSchema = yup.object({
 
   phoneNumber: yup
     .string()
-    .matches(/^\+?\d{10,15}$/, "Phone number must contain numbers only")
+    .min(10, "Phone number must be at least 10 characters")
+    .max(14, "Phone number must be at most 14 characters")
+    .matches(/^\+?\d{9,15}$/, "Phone number must contain numbers only")
     .required("Please enter a valid phone number"),
 
   address: yup
@@ -23,4 +25,3 @@ export const validationSchema = yup.object({
     .required("Please enter your address")
     .min(3, "Address must be at least 3 characters"),
 });
-
--- src/pages/SupplierDetails/components/PersonalSupplierInfo/formSchema.ts
+++ src/pages/SupplierDetails/components/PersonalSupplierInfo/formSchema.ts
@@ -4,12 +4,14 @@ export const validationSchema = yup.object({
   supplierName: yup
     .string()
     .required("Please enter your full name")
-    .min(2, "Full name must be at least 2 characters").min(2, "Name must be at least 2 characters")
+    .min(2, "Full name must be at least 2 characters")
+    .min(2, "Name must be at least 2 characters")
     .matches(
       /^[A-Za-z\u0600-\u06FF\s]+$/,
-      "Name must contain letters only (Arabic or English)"),
+      "Name must contain letters only (Arabic or English)"
+    ),
 
-    email: yup
+  email: yup
     .string()
     .email("Please enter a valid email")
     .required("Please enter your email")
@@ -18,7 +20,9 @@ export const validationSchema = yup.object({
 
   phoneNumber: yup
     .string()
-    .matches(/^\+?\d{10,15}$/, "Phone number must contain numbers only")
+    .min(10, "Phone number must be at least 10 characters")
+    .max(14, "Phone number must be at most 14 characters")
+    .matches(/^\+?\d{9,15}$/, "Phone number must contain numbers only")
     .required("Please enter a valid phone number"),
 
   address: yup
@@ -26,4 +30,3 @@ export const validationSchema = yup.object({
     .required("Please enter your address")
     .min(3, "Address must be at least 3 characters"),
 });
-
