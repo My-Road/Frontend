288b43f04c3c2a2e046dc76782ed027eedcdead9
BaselTurkman <Basel.Turkman123@gmail.com>
2025-05-21 20:42:13 +0300

    DP-40 Enhance design for Suppliers page & fix endpoints urls

--- src/pages/Suppliers/API/index.ts
+++ src/pages/Suppliers/API/index.ts
@@ -4,7 +4,7 @@ import { SearchParams } from "@/types";
 
 export const addSupplierAPI = async (payload: AddSupplierPayLoad) => {
   const res = await axiosInstance.post<AddSupplierPayLoad>(
-    "/api/v1/supplier",
+    "/api/v1/suppliers",
     payload
   );
   return res.data;
@@ -14,13 +14,13 @@ export const searchSuppliersAPI = async (
   params: SearchParams
 ): Promise<SearchResponseForSuppliers> => {
   const res = await axiosInstance.post<SearchResponseForSuppliers>(
-    "/api/v1/supplier/search",
+    "/api/v1/suppliers/search",
     params
   );
   return res.data;
 };
 
 export const resetSupplierAPI = async (id: number) =>{
-  const res = await axiosInstance.put(`/api/v1/supplier/restore/${id}`)
+  const res = await axiosInstance.put(`/api/v1/suppliers/restore/${id}`)
   return res.data;
 }
--- src/pages/Suppliers/Suppliers.tsx
+++ src/pages/Suppliers/Suppliers.tsx
@@ -1,10 +1,19 @@
-import { Container, Stack } from "@mui/material";
+import {
+  Box,
+  Container,
+  Divider,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
 import AddSupplierForm from "./components/AddSupplierForm";
 import SupplierDataGrid from "./components/SupplierDataGrid";
 import { useState } from "react";
 import { SearchParams } from "@/types";
 import { DEFAULT_SEARCH_PARAMS } from "@/constants";
 import SearchFormByName from "@/components/SearchFormByName";
+import { Trans } from "react-i18next";
+import GroupsTwoToneIcon from "@mui/icons-material/GroupsTwoTone";
 
 export default function Suppliers() {
   const [searchParams, setSearchParams] = useState<SearchParams>({
@@ -12,15 +21,31 @@ export default function Suppliers() {
     sorts: "isDeleted",
   });
   return (
-    <Container>
+    <Container sx={{ my: 5 }}>
       <Stack gap={4}>
         <AddSupplierForm />
-        <SearchFormByName
-          setSearchParams={setSearchParams}
-          name="supplierName"
-          sortsBy="isDeleted"
-        />
-        <SupplierDataGrid searchParams={searchParams} />
+        <Paper elevation={3} sx={{ p: 3, borderRadius: 3 }}>
+          <Typography
+            variant="h5"
+            fontWeight={600}
+            gutterBottom
+            display="flex"
+            alignItems="center"
+            gap={1}
+          >
+            <GroupsTwoToneIcon fontSize="large" />
+            <Trans i18nKey="SideDrawerLinks.Suppliers">Suppliers</Trans>
+          </Typography>
+          <Divider />
+          <Box mb={4}>
+            <SearchFormByName
+              setSearchParams={setSearchParams}
+              name="supplierName"
+              sortsBy="-isDeleted"
+            />
+          </Box>
+          <SupplierDataGrid searchParams={searchParams} />
+        </Paper>
       </Stack>
     </Container>
   );
