ec926d7b4a8f264615a8448aa0cf9730a1448de2
BaselTurkman <Basel.Turkman123@gmail.com>
2025-06-15 14:44:04 +0300

    DP-65 Handle Reports on Customer Orders

--- src/pages/Reports/Customers/API/index.ts
+++ src/pages/Reports/Customers/API/index.ts
@@ -2,10 +2,24 @@ import { axiosInstance } from "@/config/axios.config";
 import { SearchParams } from "@/types";
 import { SearchResponseForOrders } from "../types";
 
-export const searchCustomersOrdersAPI = async ( params?: SearchParams) => {
+export const searchCustomersOrdersAPI = async (params?: SearchParams) => {
   const res = await axiosInstance.post<SearchParams>(
     `/api/v1/order/search`,
     params
   );
   return res.data as SearchResponseForOrders;
+};
+
+export const downloadCustomerReport = async (params?: SearchParams) => {
+  const res = await axiosInstance.post(`/api/v1/report/orders-report`, params, {
+    responseType: "blob",
+  });
+
+  const url = window.URL.createObjectURL(new Blob([res.data]));
+  const link = document.createElement("a");
+  link.href = url;
+  link.setAttribute("download", "customer_orders_report.pdf");
+  document.body.appendChild(link);
+  link.click();
+  link.remove();
 };
