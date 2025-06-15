50db0eb283b04574dee9d780a482fb8e677f4b53
BaselTurkman <Basel.Turkman123@gmail.com>
2025-06-15 14:44:52 +0300

    DP-65 Handle Reports on Supplier Purchases

--- src/pages/Reports/Purchases/API/index.ts
+++ src/pages/Reports/Purchases/API/index.ts
@@ -9,3 +9,17 @@ export const searchPurchasesAPI = async ( params?: SearchParams) => {
   );
   return res.data as SearchResponseForPurchases;
 };
+
+export const downloadPurchasesReport = async (params?: SearchParams) => {
+  const res = await axiosInstance.post(`/api/v1/report/purchase-report`, params, {
+    responseType: "blob",
+  });
+
+  const url = window.URL.createObjectURL(new Blob([res.data]));
+  const link = document.createElement("a");
+  link.href = url;
+  link.setAttribute("download", "Purchases_report.pdf");
+  document.body.appendChild(link);
+  link.click();
+  link.remove();
+};
