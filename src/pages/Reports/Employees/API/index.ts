f981dd3a97007cd7bfce5f1c32d9d9f513669e87
BaselTurkman <Basel.Turkman123@gmail.com>
2025-06-15 14:44:25 +0300

    DP-65 Handle Reports on Employee Logs

--- src/pages/Reports/Employees/API/index.ts
+++ src/pages/Reports/Employees/API/index.ts
@@ -9,3 +9,17 @@ export const searchEmployeesLogsAPI = async ( params?: SearchParams) => {
   );
   return res.data as SearchResponseForLogs;
 };
+
+export const downloadEmployeesReport = async (params?: SearchParams) => {
+  const res = await axiosInstance.post(`/api/v1/report/employeesLogs-report`, params, {
+    responseType: "blob",
+  });
+
+  const url = window.URL.createObjectURL(new Blob([res.data]));
+  const link = document.createElement("a");
+  link.href = url;
+  link.setAttribute("download", "EmployeeLogs_report.pdf");
+  document.body.appendChild(link);
+  link.click();
+  link.remove();
+};
