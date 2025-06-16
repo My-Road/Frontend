50ea61ed2cc93ad545ca9b296dae081d44327928
BaselTurkman <Basel.Turkman123@gmail.com>
2025-06-16 19:22:04 +0300

    DP-68 Handle Home page for a manager

--- src/pages/Home/components/SummarySection.tsx
+++ src/pages/Home/components/SummarySection.tsx
@@ -1,33 +1,41 @@
-import {  Typography, Box } from "@mui/material";
+import { Typography, Box } from "@mui/material";
 import { Grid2 as Grid } from "@mui/material";
 import { useGetDashboardAPI } from "../hooks/useGetDashboardAPI";
 import { useTranslation } from "react-i18next";
-import { getSummaryData ,StyledSummaryCard  } from "../constants";
+import { bgColors, getSummaryData, iconColors, StyledSummaryCard } from "../constants";
 import { Grow } from "@mui/material";
 import Loader from "@/components/Loader";
+import { useAppSelector } from "@/store";
+import { isManagerRole } from "@/features/User";
 
 const SummarySection = () => {
   const { data, isLoading } = useGetDashboardAPI();
   const { t } = useTranslation();
+  const isManager = useAppSelector(isManagerRole);
 
   if (isLoading || !data) {
-    return <Loader/>;
+    return <Loader />;
   }
 
-  const summaryData = getSummaryData(t).map((item) => ({
-    label: item.label,
-    value: data[item.valueKey as keyof typeof data],
-    Icon: item.icon,
-  }));
+  const summaryData = getSummaryData(t)
+    .filter((item) => {
+      const managerKeys = ["employeeCount", "supplierCount", "customerCount"];
+      return isManager ? managerKeys.includes(item.valueKey) : true;
+    })
+    .map((item) => ({
+      label: item.label,
+      value: data[item.valueKey as keyof typeof data],
+      Icon: item.icon,
+    }));
 
   return (
     <Grid container spacing={3}>
       {summaryData.map(({ label, value, Icon }, index) => (
-        <Grid key={label + index} size={{ xs: 12, sm: 6, md: 4 }}>
+        <Grid key={label + index} size={{ xs: 12, sm: 6, md: 4 }} >
           <Grow in={true} timeout={500 + index * 200}>
-            <StyledSummaryCard elevation={3}>
+            <StyledSummaryCard elevation={3} bgcolor={bgColors[index % bgColors.length]}>
               <Box
-                bgcolor="primary.main"
+                bgcolor={iconColors[index % iconColors.length]}
                 color="white"
                 p={1.5}
                 borderRadius={2}
@@ -39,7 +47,6 @@ const SummarySection = () => {
               >
                 <Icon fontSize="medium" />
               </Box>
-
               <Box>
                 <Typography variant="subtitle2" color="text.secondary">
                   {label}
