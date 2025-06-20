68eba3c86868a1a5e7882be29b1edb2cdcb9b80b
BaselTurkman <Basel.Turkman123@gmail.com>
2025-06-20 16:08:10 +0300

    DP-70 Add config for axiso

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { getSession } from "@/lib/session";
+import { clearSession, getSession } from "@/lib/session";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -18,6 +18,17 @@ axiosInstance.interceptors.request.use((config) => {
   }
   return config;
 });
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      clearSession();
+      window.location.href = "/unauthenticated";
+    }
+    return Promise.reject(error);
+  }
+);
 
 export const axiosFormData = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
